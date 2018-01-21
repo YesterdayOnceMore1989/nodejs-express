4cb0f2c9cd49d16e2986450e8370604add3edd86
YesterdayOnceMore1989 <32479680+YesterdayOnceMore1989@users.noreply.github.com>
2018-01-21 15:30:28 +0700

    change url

--- server.js
+++ server.js
@@ -88,9 +88,7 @@ app.get('/about', (req, res)=>{
 });
 
 app.get('/bad', (req, res)=>{
-  res.send({
-    errorMessage: 'Unable to handle request'
-  });
+  res.render('test.hbs');
 });
 
 app.get('/help', (req, res)=>{
